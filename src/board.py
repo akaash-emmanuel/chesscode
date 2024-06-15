615423b3cbe5a72a8dbc50ddd8e724f1d75a4aa9
Akaash <117347573+akaash-emmanuel@users.noreply.github.com>
2024-06-15 14:14:23 +0530

    knight valid moves

--- src/board.py
+++ src/board.py
@@ -1,6 +1,7 @@
 from constant import *
 from square import Square
 from piece import *
+from move import Move
 
 class Board:
     def __init__(self):
@@ -8,6 +9,50 @@ class Board:
         self._create()
         self._add_pieces('white')
         self._add_pieces('black')
+
+# to calculate all valid moves of a piece on a position
+    def calc_moves(self, piece, row, col):
+
+        def knight_moves():
+            possible_moves = [
+                (row-2, col +1),
+                (row-1, col+2),
+                (row+1, col+2),
+                (row+2, col+1),
+                (row+2, col-1),
+                (row+1, col-2),
+                (row-1, col-2),
+                (row-2, col-1),
+            ]
+            for possible_move in possible_moves:
+                possible_move_row, possible_move_col = possible_move
+
+                if Square.in_range(possible_move_row, possible_move_col):
+                    if self.squares[possible_move_row, possible_move_col].isempty_or_rival(piece.color):        # if the valid square is empty, its possible, otherwise it isnt
+                        
+                        initial =   Square(row, col)
+                        final = Square(possible_move_row, possible_move_col)
+
+                        move = Move(initial, final)
+                        piece.add_move(move)
+
+        if piece.name == 'pawn':
+            pass
+
+        elif piece.name == 'knight':
+            knight_moves()
+
+        elif piece.name == 'bishop':
+            pass
+
+        elif piece.name == 'rook':
+            pass
+
+        elif piece.name == 'queen':
+            pass
+
+        elif piece.name == 'king':
+            pass
 
     def _create(self):  
         for row in range(rows):
---
+++ src/move.py
@@ -0,0 +1,4 @@
+class Move:
+    def __init__(self, initial, final):
+        self.initial = initial              #initial and final squares
+        self.final = final
